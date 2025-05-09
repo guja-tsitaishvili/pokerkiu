8a6602f20ff30a5000c7e698e1f5565eaaf3db0d
guja-tsitaishvili <elgujatsitaishvili60@gmail.com>
2025-05-09 23:13:30 +0400

    development branch 11:13 5/9

---
+++ frontend/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'  // or App.tsx if you're using TS
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
