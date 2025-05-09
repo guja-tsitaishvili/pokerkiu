54ebfe8b769e53236a9c6e7eccb0d4ddd38c1e6f
guja-tsitaishvili <elgujatsitaishvili60@gmail.com>
2025-05-09 23:49:42 +0400

    this is after image and running website 5/9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import pokerTable from '/public/pics/pokertable.jpg';
+
+import './App.css';
 function App() {
-  return <h1>Hello from App!</h1>
+  return <>
+  <h1>Hello from App!</h1>
+  <img src={pokerTable} className="image" alt="Poker Table"/>
+  
+  </>
+
 }
 
 export default App
+
+
+
